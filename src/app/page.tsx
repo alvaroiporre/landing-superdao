c579be788b2740053d0d46081cbe139e3e160e65
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-11 16:45:25 -0400

    Add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <main className="">
       
       <Navbar />
-     <div>main</div>
+      <Hero />
     </main>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div className="p-[1rem] text-center my-10">
+      <h1 className="text-[2.5rem] font-bold">The easiest way to start a DAO</h1>
+      <p className="text-[1rem] font-extralight my-2">An all-in-one platform to start, manage and grow a decentralized autonomous organization</p>
+      <button className="bg-[#ffcf01] text-[#0A1117] px-[1.5rem] py-[0.75rem] rounded-xl my-5">Get started</button>
+    </div>
+  );
+};
+export default Hero;
